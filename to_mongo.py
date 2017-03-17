6b85f442cd261271d1649d7719a014d9efd5eed4
David Kane <d.r.kane@gmail.com>
2017-03-17 10:19:53 +0000

    Use command line arguments for to_mongo.py

--- to_mongo.py
+++ to_mongo.py
@@ -1,22 +1,47 @@
+from __future__ import print_function
 from pymongo import MongoClient
+import argparse
 import json
+import os
+import urllib.request
 
-client = MongoClient('localhost', 27017)
-db = client['360giving']
+def main():
 
+    parser = argparse.ArgumentParser(description='Import grantnav file to mongo db')
+    parser.add_argument('--grantnav', default='http://grantnav.threesixtygiving.org/api/grants.json', help='Location of grantnav file downloaded from <http://grantnav.threesixtygiving.org/api/grants.json>')
+    parser.add_argument('--mongo-port', '-p', type=int, default=27017, help='Port for mongo db instance')
+    parser.add_argument('--mongo-host', '-mh', default='localhost', help='Host for mongo db instance')
+    parser.add_argument('--mongo-db', '-db', default='360giving', help='Database to import data to')
+    parser.add_argument('--limit', '-l', type=int, default=10000, help='Size of chunks imported in bulk')
+    args = parser.parse_args()
 
-grantnav = 'grantnav-20170301150836.json'
+    client = MongoClient(args.mongo_host, args.mongo_port)
+    db = client[args.mongo_db]
+    print("Connected to '%s' mongo database [host: %s, port: %s]" % (args.mongo_db, args.mongo_host, args.mongo_port))
 
-with open(grantnav) as g:
-    grants = json.load(g)
+    if os.path.isfile(args.grantnav):
+        grantnav = args.grantnav
+        print("Using existing file: %s" % args.grantnav)
+    else:
+        grantnav = "grants.json"
+        print("Downloading from: %s" % args.grantnav)
+        urllib.request.urlretrieve(args.grantnav, grantnav)
+        print("Saved as: %s" % grantnav)
 
-bulk_limit = 10000
-bulk = []
-for i in grants["grants"]:
-    bulk.append(i)
-    if len(bulk)>=bulk_limit:
+    with open(grantnav) as g:
+        grants = json.load(g)
+
+    bulk = []
+    for k, i in enumerate(grants["grants"]):
+        bulk.append(i)
+        if len(bulk)>=args.limit:
+            db.grants.insert_many(bulk)
+            print("%s records inserted, %s total" % (len(bulk), k))
+            bulk = []
+
+    if len(bulk)>0:
         db.grants.insert_many(bulk)
-        bulk = []
+        print("%s records inserted, %s total" % (len(bulk), k))
 
-if len(bulk)>0:
-    db.grants.insert_many(bulk)
+if __name__ == '__main__':
+    main()
