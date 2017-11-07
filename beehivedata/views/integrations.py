c54a76a11ac25b7c3e21752266fa8af7b0104d7c
David Kane <d.r.kane@gmail.com>
2017-11-07 16:54:13 +0000

    Better funder description

--- beehivedata/views/integrations.py
+++ beehivedata/views/integrations.py
@@ -8,6 +8,7 @@ from ..assets.queries.fund_summary import fund_summary_query, process_fund_summa
 from ..assets.queries.amounts import amounts_query
 from ..assets.queries.durations import durations_query
 from ..assets.queries.themes import themes_query
+from ..assets.name_parse import parse_name
 
 integrations = Blueprint('integrations', __name__)
 
@@ -84,4 +85,52 @@ def funds():
             conditions[i] = {"$in": request.values.get(i).split(",")}
 
     grant_makers = db.charities.find(conditions, sort=[("grants_made", -1)], limit=50)
-    return jsonify(list(grant_makers))
+
+    grant_makers = [get_funder_info(g) for g in grant_makers]
+
+    return jsonify(grant_makers)
+
+
+def get_funder_info(funder):
+
+    funder["name"] = parse_name(funder["name"])
+
+    new_funder = {
+        "name": funder["name"],
+        "reg_number": funder["reg_number"],
+        "countries": list(set([a["iso3166_1"] for a in funder.get("areas", [])])),
+        "districts": list(set([a["iso3166_2_GB"] for a in funder.get("areas", []) if a["iso3166_2_GB"] != ""])),
+        "geo_area": funder.get("geo_area")
+    }
+
+    description = ""
+
+    # location
+    location = funder.get("geo_area", funder["operating_location"].lower())
+    if location is None:
+        location = funder.get("operating_location", "").lower()
+    elif location.lower() == "worldwide":
+        location = "worldwide"
+    else:
+        location = "in {}".format(location)
+
+    # themes
+    themes = ""
+    if len(funder.get("purpose", [])) == 1:
+        themes = " on {}.".format(funder["purpose"][0].lower())
+    elif len(funder.get("purpose", [])) > 1:
+        themes = "\r\n".join([" - {}".format(i[0].upper() + i[1:].lower())
+                for i in funder["purpose"]])
+        themes = " on the following themes: \r\n\r\n{}\r\n".format(themes)
+
+    description += "{} works {}{}\r\n\r\n".format(
+        funder["name"], 
+        location,
+        themes
+    )
+
+    # grant amounts
+    description += "In the year ending {:%B %Y} they made grants worth £{:,.0f}.".format(funder["fye"], funder["grants_made"])
+    new_funder["description"] = description
+
+    return new_funder
